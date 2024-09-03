970b225e9e1b0a16602fc6fda40f2efe61255159
Faruq Bakare <bakarefaruq.a@gmail.com>
2024-09-03 19:14:29 +0100

    minor

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -43,7 +43,7 @@ export default function Page() {
     createTaskHelper,
     {
       onSuccess: () => {
-        // mutate("/api/user");
+        // invalidate the date cache
       },
       onError: (error) => {
         // console.error("error", error.message);

af7f1cd5c85cda43997ff14a93414593bb3cb3a7
Faruq Bakare <bakarefaruq.a@gmail.com>
2024-09-04 15:54:01 +0100

    prisma migration completed

---
+++ migrations/20240904144952_init/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "status" TEXT NOT NULL,
+    "priority" TEXT,
+    "date" TEXT NOT NULL,
+    "cover" TEXT,
+    "deadline" TIMESTAMP(3) NOT NULL,
+    "tracker" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
